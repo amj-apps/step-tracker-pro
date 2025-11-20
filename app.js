af0e66672ccf30e0da83858859cd6b74dd0e29b9
agus marjan <agusmarjan@iMac119.local>
2025-11-20 10:08:52 +0700

    add more threshold

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 // ==========================================================
 
 // Sensor and Calculation Constants
-const ACCELERATION_THRESHOLD = 1.25; 
-const STEP_DEBOUNCE_TIME = 300;
+const ACCELERATION_THRESHOLD = 1.75; 
+const STEP_DEBOUNCE_TIME = 200;
 const STEP_LENGTH_M = 0.76;
 
 // Element References (Defined once, globally)
